baa182cdcd60f7bed2c6e53255e774757d4fba28
Nathaniel van Diepen <eeems@omnimaga.org>
2015-03-19 16:59:10 -0600

    canvas.js rework

--- www/js/canvas.js
+++ www/js/canvas.js
@@ -1,3 +1,129 @@
 (function(global,undefined){
-	
+	var layers = [],
+		font;
+	global.extend({
+		Canvas: function(name){
+			var node = dom.create('canvas').attr({id:'canvas_'+now,name:'canvas_'+name}),
+				context = node[0].getContext('2d');
+			this.extend({
+				id: new Prop({
+					get: function(){
+						return node[0].id;
+					},
+					set: function(val){
+						node.attr({id:val});
+					}
+				}),
+				name: new Prop({
+					get: function(){
+						return name;
+					},
+					set: function(val){
+						name = val;
+						node.attr({name:'canvas_'+name});
+					}
+				}),
+				node: new Prop({
+					get: function(){
+						return node;
+					}
+				}),
+				context: new Prop({
+					get: function(){
+						return context;
+					}
+				}),
+				font: new Prop({
+					get: function(){
+						return context.font;
+					},
+					set: function(val){
+						context.font = val;
+					}
+				}),
+				baseline: new Prop({
+					get: function(){
+						return context.textBaseline;
+					},
+					set: function(val){
+						context.textBaseline = val;
+					}
+				}),
+				align: new Prop({
+					get: function(){
+						return context.textAlign;
+					},
+					set: function(val){
+						context.textAlign = val;
+					}
+				}),
+				style: function(style){
+					for(var i in style){
+						try{
+							if(context[i]!==undefined){
+								context[i] = style[i];
+							}
+						}catch(e){
+							console.warn(e);
+						}
+					}
+					return this;
+				},
+				stroke: function(){
+					context.stroke();
+					return this;
+				},
+				fill: function(){
+					context.fill();
+					return this;
+				},
+				width: new Prop({
+					get: function(){
+						return node.width();
+					},
+					set: function(val){
+						node.width(val);
+					}
+				}),
+				height: new Prop({
+					get: function(){
+						return node.height();
+					},
+					set: function(val){
+						node.height(val);
+					}
+				}),
+				text: function(text,x,y,w){
+					context.fillText(text,x,y,w);
+					return this;
+				}
+			});
+		},
+		canvas: new Module({
+			layers: new Prop({
+				get: function(){
+					return layers;
+				}
+			}),
+			get: function(name){
+				var layer;
+				layers.each(function(){
+					if(this.name == name){
+						layer = this;
+					}
+				});
+				if(!layer){
+					layer = new Canvas(name);
+					layers.push(layer);
+				}
+				return layer;
+			},
+			each: function(fn){
+				for(var i=0;i<layers.length;i++){
+					fn.call(layers[i],i);
+				}
+				return this;
+			}
+		})
+	});
 })(window);
