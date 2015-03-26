88bd9245825789f00a939f6c5e1388f1134fddfb
Nathaniel van Diepen <eeems@omnimaga.org>
2015-03-25 20:35:48 -0600

    server stuff

--- bin/server.js
+++ bin/server.js
@@ -1,157 +1,28 @@
-var fs = require('fs'),
-	app = require('node-static'),
-	http = require('http'),
-	ws = require('ws'),
-	config = require('../etc/server.json');
+var config = require('../etc/server.json'),
+	SocketServer = require('../lib/socketserver.js').SocketServer,
+	app = new SocketServer(config.data,config.host,config.port,config['ws-host'],config['ws-port']),
+	wss = app.WebSocketServer,
+	fs = require('fs');
 process.chdir(__dirname+'/..');
-console.log('Running from context %s',process.cwd());
-exports.start = function(folder,port,host,ws_host,ws_port){
-	console.log('Starting server for %s on %s:%d',folder,host,port);
-	console.log('Starting websocket server on %s:%d',ws_host,ws_port);
-	var file = new app.Server(folder,{
-			gzip: true,
-			serverInfo: 'juju/node-static/0.0.1'
-		}),
-		sockets = [],
-		handle = {
-			_handles: [],
-			add: function(name,callback,scope){
-				this._handles[name.toUpperCase()] = function(){
-					var r = callback.apply(this,arguments);
-					return r === undefined?'':r;
+app.path('/etc/settings.json',function(req,res){
+	res.writeHead(200,{
+		"Content-Type": 'application/json'
+	});
+	fs.readFile('etc/client.json',function(e,d){
+		if(!e){
+			try{
+				d = JSON.parse(d);
+				d.websocket = {
+					host: ws_host,
+					port: ws_port
 				};
-				return this;
-			},
-			get: function(name){
-				var f = this._handles[name.toUpperCase()];
-				return f===undefined?function(data){
-					console.log('DEBUG(%s) - %s',name,data);
-					return '';
-				}:f;
-			},
-			run: function(name,data,scope){
-				return this.get(name).call(scope,data);
-			}
-		},
-		wss = new ws.Server({
-			host: ws_host,
-			port: ws_port
-		}),
-		server = http.createServer(function(req,res){
-			if(req.url == '/etc/settings.json'){
-				console.log('Serving client settings');
-				res.writeHead(200,{
-					"Content-Type": 'application/json'
-				});
-				fs.readFile('etc/client.json',function(e,d){
-					if(!e){
-						try{
-							d = JSON.parse(d);
-							console.log('  Settings have no errors');
-							d.websocket = {
-								host: ws_host,
-								port: ws_port
-							};
-							res.end(JSON.stringify(d));
-						}catch(err){
-							e = err;
-						}
-					}
-					if(e){
-						console.log('  Settings have errors');
-						console.error(e);
-						res.end("{\"debug\": false}");
-					}
-				});
-			}else if(req.url.substr(0,7) == '/debug/'){
-				var type = req.url.substr(7,req.url.indexOf('/',7)-7),
-					data = decodeURIComponent(req.url.substr(req.url.indexOf('/',7)+1));
-				res.write(handle.run(type,data,res));
-				res.end();
-			}else{
-				console.log('Serving '+req.url);
-				file.serve(req,res);
-			}
-		}).listen(port,host,2048);
-	wss.broadcast = function(data){
-		for(var i in sockets){
-			if(sockets[i].readyState == ws.OPEN){
-				sockets[i].send(data);
+				res.end(JSON.stringify(d));
+			}catch(err){
+				e = err;
 			}
 		}
-	};
-	wss.on('connection',function(ws){
-		console.log('SOCKET OPENED');
-		ws.on('message',function(msg){
-			console.log('SOCKET DEBUG: %s',msg);
-			handle.run(msg.substr(0,msg.indexOf(' ')),msg.substr(msg.indexOf(' ')+1),ws);
-		});
-		ws.on('close',function(){
-			if(ws.fingerprint !== undefined){
-				console.log('SOCKET CLOSED '+ws.fingerprint);
-				wss.broadcast('QUIT '+ws.fingerprint);
-			}else{
-				console.log('SOCKET CLOSED');
-			}
-			if(ws.timeout !== undefined){
-				clearInterval(ws.timeout);
-			}
-			sockets.splice(sockets.indexOf(ws),1);
-		});
-		ws.timeout = setInterval(function(){
-			ws.send('PING '+(+new Date));
-		},100000);
-		console.log('SOCKET CONNECTION COUNT: '+sockets.length);
+		if(e){
+			res.end("{\"debug\": false}");
+		}
 	});
-	handle.add('fingerprint',function(data){
-			for(var i in sockets){
-				if(sockets[i].fingerprint == data){
-					console.log('SOCKET KILL - '+data+'  - Already Connected');
-					this.close(3000,'Already Connected');
-					return;
-				}
-			}
-			this.fingerprint = data;
-			sockets.push(this);
-			wss.broadcast('JOIN '+data);
-			console.log('SOCKET REGISTER - '+data);
-		})
-		.add('page',function(data){
-			data = JSON.parse(data);
-			this.page = data;
-			console.log('SOCKET PAGE - '+this.fingerprint+' - '+data.href);
-		})
-		.add('ping',function(data){
-			return 'PONG '+data;
-		})
-		.add('pong',function(data){
-			console.log('SOCKET PING - %s - %dms',ws.fingerprint,((+new Date)-parseInt(data)));
-		})
-		.add('log',function(data){
-			console.log('DEBUG - %s',data);
-		})
-		.add('sockets',function(data){
-			var s = [],
-				i;
-			for(i in sockets){
-				s.push({
-					fingerprint: sockets[i].fingerprint,
-					page: sockets[i].page
-				});
-			}
-			return JSON.stringify(s);
-		});
-	return {
-		server: server,
-		WebSocketServer: wss,
-		sockets: sockets,
-		handle: handle
-	};
-};
-
-if(!module.parent){
-	global.ws = exports.start(config.data,config.port,config.host,config['ws-host'],config['ws-port']);
-	require('repl').start({
-		useGlobal: true
-	});
-}
+});
