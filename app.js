c7d2df3bc06563bcdc3d2d8d1beb748a1ab61fb2
Nathaniel van Diepen <eeems@omnimaga.org>
2015-12-16 18:05:20 -0700

    Loading improvements

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
 (function(global,undefined){
 	"use strict";
 	global.ready(function(){
-		widget.require([
-			'header',
-			'nav',
-			'section',
-			'footer',
-			'link',
-			'input',
-			'text',
-			'button',
-			'save'
-		]).then(function(){
-			dom.body.append(
-				widget.new.section()
-					.add({
-						type: 'input',
-						name: 'alert-value',
-						label: 'Alert value:'
-					})
-					.add({
-						type: 'button',
-						label: 'Display Alert',
-						events:{
-							click: function(){
-								alert(dom.get('[name=alert-value]').val());
-							}
+		dom.body.append(
+			widget.new.section()
+				.add({
+					type: 'input',
+					name: 'alert-value',
+					label: 'Alert value:'
+				})
+				.add({
+					type: 'button',
+					label: 'Display Alert',
+					events:{
+						click: function(){
+							alert(dom.get('[name=alert-value]').val());
 						}
-					})
-					.render()
-					.body
-			);
-		})
-		.catch(function(e){
-			console.error(e);
-		});
+					}
+				})
+				.render()
+				.body
+		);
 	});
 })(window);
--- init.js
+++ init.js
@@ -1,40 +1,64 @@
 (function(global,undefined){
 	"use strict";
 	var inc = function(path,onload){
-		var script = document.createElement('script');
-		script.src = path;
-		script.onload = onload;
-		document.head.appendChild(script);
-	};
+			console.info('INC('+path+')');
+			var script = document.createElement('script');
+			script.src = path;
+			script.onload = onload;
+			document.head.appendChild(script);
+		};
+	console.group('Startup');
+	console.time('Startup');
+	console.group('Inc');
+	console.time('Inc');
 	inc('lib/core.js',function(){
+		var version = global.settings.version;
 		inc('lib/fetch.js',function(){
+			console.timeEnd('Inc');
+			console.groupEnd();
+			console.group('Init');
+			console.time('Init');
 			fetch('app.json')
 				.then(function(res){
+					console.info('INIT(app.json)');
 					return res.json();
 				})
 				.then(function(json){
-					global.settings = json;
-					require([
-						'lib/dom.js',
-						'lib/canvas.js',
-						'lib/keyboard.js',
-						'lib/mouse.js',
-						'lib/page.js',
-						'lib/socket.js',
-						'lib/db.js',
-						'lib/cookie.js',
-						'lib/widget.js',
-						'app.js'
-					]).then(function(){
-						if(global instanceof Window && document.readyState != 'complete'){
-							global.addEventListener('load',global.ready);
-						}else{
-							global.ready();
-						}
-					})
-					.catch(function(e){
-						console.error(e);
-					});
+					for(var i in json.settings){
+						global.settings[i] = json.settings[i];
+					}
+					json.include = json.include || [];
+					json.include.push(json.init || 'app.js');
+					console.group('Require');
+					console.group('Require.include');
+					console.time('Require.include');
+					return require(json.include,version!=global.settings.version)
+						.then(function(){
+							console.timeEnd('Require.include');
+							console.groupEnd();
+							console.group('Require.widget');
+							console.time('Require.widget');
+							return widget.require(json.widgets)
+						});
+				})
+				.then(function(){
+					console.timeEnd('Require.widget');
+					console.groupEnd();
+					console.groupEnd();
+					console.timeEnd('Init');
+					console.groupEnd();
+					if(global instanceof Window && document.readyState != 'complete'){
+						global.addEventListener('load',global.ready);
+					}else{
+						global.ready();
+					}
+					console.timeEnd('Startup');
+					console.groupEnd();
+				})
+				.catch(function(e){
+					console.error(e);
+					console.timeEnd('Startup');
+					console.groupEnd();
 				});
 		});
 	})
